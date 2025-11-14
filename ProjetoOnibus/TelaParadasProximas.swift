ac41632d4ef62dc1c9c9b0cf228ba86fab3c8894
Turma02-2 <turma02-2@HackaTruck02.local>
2025-11-14 16:35:25 -0300

    add latitude and longitude params for stopbus

--- ProjetoOnibus/TelaParadasProximas.swift
+++ ProjetoOnibus/TelaParadasProximas.swift
@@ -11,13 +11,15 @@ struct Paradas: Identifiable, Hashable {
     var id: Int
     var nome: String
     var distancia: Int
+    var latitude: Double
+    var longitude: Double
 }
 
 struct TelaParadasProximas: View {
     @State var paradas: [Paradas] = [
-        Paradas(id: 1, nome: "Avenida Washington Soares - Passarela Unifor", distancia: 700),
-        Paradas(id: 2, nome: "Avenida Washington Soares - Edson Queiroz, Fortaleza - CE, 60811-025 - Unifor", distancia: 99),
-        Paradas(id: 3, nome: "Avenida Valmir Pontes - Em frente ao NAMI", distancia: 66)
+        Paradas(id: 1, nome: "Avenida Washington Soares - Passarela Unifor", distancia: 700, latitude: -3.7689661, longitude: -38.4827782),
+        Paradas(id: 2, nome: "Avenida Washington Soares - Edson Queiroz - Unifor", distancia: 99, latitude: -3.7708599, longitude: -38.4819135),
+        Paradas(id: 3, nome: "Avenida Valmir Pontes - NAMI", distancia: 66, latitude: -3.771173, longitude: -38.4810759)
     ]
     
     var paradasOrdenadas: [Paradas] {
